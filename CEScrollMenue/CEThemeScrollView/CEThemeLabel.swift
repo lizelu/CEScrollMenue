f530e89089865a0d2e82d11ae041a8d9592f69cd
zeluliCredit <zeluli@creditease.cn>
2017-03-24 18:59:31 +0800

    init

---
+++ CEScrollMenue/CEThemeScrollView/CEThemeLabel.swift
@@ -0,0 +1,21 @@
+//
+//  CEThemeLabel.swift
+//  CEScrollMenue
+//
+//  Created by Mr.LuDashi on 2017/3/24.
+//  Copyright © 2017年 ZeluLi. All rights reserved.
+//
+
+import UIKit
+
+class CEThemeLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CEScrollMenue/CEThemeScrollView/CEThemeScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  CEThemeScrollView.swift
+//  CEScrollMenue
+//
+//  Created by Mr.LuDashi on 2017/3/24.
+//  Copyright © 2017年 ZeluLi. All rights reserved.
+//
+
+import UIKit
+
+class CEThemeScrollView: UIScrollView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.green
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
