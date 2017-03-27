696319cfbac1d4932a29e4fa704f598fda959cf9
zeluliCredit <zeluli@creditease.cn>
2017-03-27 17:25:23 +0800

    update source code

--- CEScrollMenue/CEThemeScrollView/CEHeaderCollectionReusableView.swift
+++ CEScrollMenue/CEThemeScrollView/CEHeaderCollectionReusableView.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class CEHeaderCollectionReusableView: UICollectionReusableView {
     var titleLabel: UILabel!
+    var editButton: CEThemeEditButton!
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.clear
         self.titleLabel = UILabel(frame: CGRect(x: 0, y: 30, width: 200, height: 20))
         self.titleLabel.font = UIFont.boldSystemFont(ofSize: 15)
         self.addSubview(titleLabel)
+        
+        self.editButton = CEThemeEditButton(frame: CGRect(x: self.frame.width - 50, y: 30, width: 40, height: 30))
+        self.editButton.addTarget(self, action: #selector(tapEditButton(sender:)), for: .touchUpInside)
+        self.addSubview(self.editButton)
+        
+    }
+    
+    func tapEditButton(sender: CEThemeEditButton) {
+        sender.isSelected = !sender.isSelected
     }
     
     required init?(coder aDecoder: NSCoder) {
---
+++ CEScrollMenue/CEThemeScrollView/CEThemeEditButton.swift
@@ -0,0 +1,36 @@
+//
+//  CEThemeEditButton.swift
+//  CEScrollMenue
+//
+//  Created by Mr.LuDashi on 2017/3/27.
+//  Copyright © 2017年 ZeluLi. All rights reserved.
+//
+
+import UIKit
+
+class CEThemeEditButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.borderWidth = 0.2
+        self.layer.cornerRadius = 10
+        self.setTitle("编辑", for: .normal)
+        self.setTitle("完成", for: .selected)
+        self.isSelected = false
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 11)
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
