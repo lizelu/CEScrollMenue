2b0ac77b5b804b696a79d7161f0f8fd44e7d5c56
zeluliCredit <zeluli@creditease.cn>
2017-03-31 10:11:04 +0800

    code review

--- CEScrollMenue/CEMenuScrollController/CEMenuScrollController.swift
+++ CEScrollMenue/CEMenuScrollController/CEMenuScrollController.swift
@@ -11,8 +11,8 @@ import UIKit
 
 class CEMenuScrollController: UIViewController, UICollectionViewDataSource {
 
-    var menuView: CEMenuView!
-    var contentCollectionView: CEContentCollectionView!
+    var menuView: CEMenuView!   //上方滑动的菜单View
+    var contentCollectionView: CEContentCollectionView!     //下方滚动的内容Cell
     
     var dataSource: DataSourceType!
     
@@ -34,39 +34,48 @@ class CEMenuScrollController: UIViewController, UICollectionViewDataSource {
         self.addContentCollectionView()
     }
     
-    func addMenuView() {
+    //Mark: - Private Method
+    
+    /// 添加菜单View
+    private func addMenuView() {
         self.menuView = CEMenuView(dataSource: self.dataSource, frame: CGRect(x: 0, y: 63, width: SCREEN_WIDTH, height: 45))
         weak var weak_self = self
         self.menuView.setTapSelectThemeClosure {
             weak_self?.presentCESelectThemeController()
         }
         
-        ///点击Cell的回调
+        ///点击每个菜单时的回调
         self.menuView.setDidSelectItemClosure { (indexPath) in
-            print("点击的第\(indexPath.row)个Cell")
-            weak_self?.dataSource = DataSourceTools.setSelcted(dataSource: self.dataSource, index: indexPath.row)
-            weak_self?.menuView.updateDataSource(data: self.dataSource)
-            
-            //点击MenuCell后移动ContentCell到指定位置
-            weak_self?.contentCollectionView.scrollToItem(at: indexPath, at: UICollectionViewScrollPosition.centeredHorizontally, animated: true)
+            weak_self?.handelMeneClickEvent(indexPath: indexPath)
         }
         
         self.view.addSubview(self.menuView)
     }
     
-    func addContentCollectionView() {
+    /// 添加下方的内容CollectionView
+    private func addContentCollectionView() {
         let y = self.menuView.frame.origin.y + self.menuView.frame.height
         self.contentCollectionView = CEContentCollectionView(frame: CGRect(x: 0, y: y, width: SCREEN_WIDTH, height: SCREEN_HEIGHT - y), data: self.dataSource[0])
         self.contentCollectionView.dataSource = self
         weak var weak_self = self
         self.contentCollectionView.setCurrentShowCellClosure { (currentShowIndexPath) in
             weak_self?.menuView.scrollToItem(indexPath: currentShowIndexPath)
-            print("当前显示的Cell的IndexPath = \(currentShowIndexPath)")
         }
         self.view.addSubview(contentCollectionView)
     }
     
-    func presentCESelectThemeController() {
+    /// 处理菜单点击的事件
+    ///
+    /// - Parameter indexPath: 点击的菜单的IndexPath
+    private func handelMeneClickEvent(indexPath: IndexPath) {
+        dataSource = DataSourceTools.setSelcted(dataSource: self.dataSource, index: indexPath.row)
+        menuView.updateDataSource(data: self.dataSource)
+        contentCollectionView.scrollToItem(at: indexPath, at: UICollectionViewScrollPosition.centeredHorizontally, animated: true)
+        
+    }
+    
+    /// 模态展示菜单编辑VC
+    private func presentCESelectThemeController() {
         let selectThemeController = CESelectThemeController(dataSource: self.dataSource)
         
         //获取更新后的DataSource
@@ -79,12 +88,14 @@ class CEMenuScrollController: UIViewController, UICollectionViewDataSource {
         self.present(selectThemeController, animated: true) {}
     }
     
-    func updateDataSource(dataSource: DataSourceType) {
+    /// 编辑后更新数据源
+    ///
+    /// - Parameter dataSource: 编辑后的数据源
+    private func updateDataSource(dataSource: DataSourceType) {
         self.dataSource = DataSourceTools.setSelcted(dataSource: dataSource, index: 0)
         self.menuView.updateDataSource(data: self.dataSource)
         self.contentCollectionView.reloadData()
     }
-
 
     
     // Mark: - UICollectionViewDataSource
@@ -112,7 +123,6 @@ class CEMenuScrollController: UIViewController, UICollectionViewDataSource {
         return cell
     }
 
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
